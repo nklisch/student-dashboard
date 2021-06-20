92c6340d32d682f79f341f63de1064bdc9707aeb
Nathan Klisch <nklisch@gmail.com>
2021-06-20 10:27:06 -0600

    merge implemented

--- backend/backend/actions/actions.py
+++ backend/backend/actions/actions.py
@@ -1,8 +1,59 @@
 from ..database.models import Repos
 from ..schemas import Repo
 from sqlalchemy.orm import Session
+from ..database import SQLBase
+from pydantic import BaseModel
+from typing import TypeVar, List, Union
+
+ModelType = TypeVar("ModelType", bound=SQLBase)
+SchemaType = TypeVar("SchemaType", bound=BaseModel)
+
+# GETS
+def get_all(
+    db: Session, filter_by: dict, model: ModelType, schema: SchemaType = None
+) -> Union[List[ModelType], List[SchemaType]]:
+    result = db.query(model).filter_by(**filter_by).all()
+    if schema:
+        return [schema.from_orm(m) for m in result]
+    return result
 
 
-def get_repos(db: Session, semester: str):
-    result = db.query(Repos).filter(Repos.semester == semester).all()
-    return [Repo.from_orm(repo) for repo in result]
+def get(
+    db: Session, filter_by: dict, model: ModelType, schema: SchemaType = None
+) -> ModelType:
+    return db.query(model).filter_by(**filter_by).one()
+
+
+# CREATES
+def create(db: Session, data: Union[dict, SchemaType], model: ModelType) -> ModelType:
+    data_db = model(**convert_schema_to_dict(data))
+    db.add(data_db)
+    db.commit()
+    db.refresh(data_db)
+    return data_db
+
+
+def create_or_update(
+    db: Session, data: Union[dict, SchemaType], model: ModelType
+) -> ModelType:
+    data_db = model(**convert_schema_to_dict(data))
+    db.merge(data_db)
+    db.commit()
+    db.refresh(data_db)
+    return
+
+
+def create_or_update_all(
+    db: Session, data: Union[List[dict], List[SchemaType]], model: ModelType
+):
+    data = [model(**convert_schema_to_dict(d)) for d in data]
+    # db.add_all(data)
+    for d in data:
+        db.merge(d)
+    db.commit()
+
+
+def convert_schema_to_dict(data):
+    if type(data) is not dict:
+        return data.dict()
+    return data
--- backend/backend/processing/automation.py
+++ backend/backend/processing/automation.py
@@ -1,29 +1,31 @@
 from .gitconnection import github
 from ..database.models import Repos, Classes
+from ..schemas import Repo
 from sqlalchemy.orm import Session
 from ..globals import determine_semester
 from fastapi import HTTPException, status
-from ..actions.actions import get_repos
+from ..actions.actions import get_all, create_or_update_all
 
 # TODO: check that repos are already in database first and/or catch error and move on
 def populate_repos(db: Session, semester: str):
-    result = db.query(Classes).filter(Classes.semester == semester).first()
+    result = db.query(Classes).filter(Classes.semester == semester).one_or_none()
     if not result:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"The current Semester of {semester} and Orginization have not been setup.",
         )
     repos = [
-        Repos(**create_repo_dict(repo, semester))
+        create_repo_dict(repo, semester)
         for repo in github.get_organization(result.gitOrganization).get_repos()
         if isTeamRepo(repo)
     ]
-    old_repos = get_repos(db=db, semester=semester)
-    if no_dup_repos(repos, old_repos):
-        db.add_all()
-        db.commit()
-        return get_repos(db=db, semester=semester)
-    return old_repos
+    create_or_update_all(db, repos, Repos)
+    # old_repos = get_repos(db=db, semester=semester)
+    # if no_dup_repos(repos, old_repos):
+    #     db.add_all()
+    #     db.commit()
+    #     return get_repos(db=db, semester=semester)
+    return get_all(db, {"semester": semester}, Repos, Repo)
 
 
 def isTeamRepo(repo):
