4aef2bb4bdad534a396070884bef516196cd64c0
Nathan Klisch <nklisch@gmail.com>
2021-08-09 15:42:27 -0600

    chart update

--- frontend/src/views/dashboard/Dashboard.js
+++ frontend/src/views/dashboard/Dashboard.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import { CChartRadar } from '@coreui/react-chartjs'
 
 const Dashboard = () => {
-  return <></>
+  return (
+    <CChartRadar
+      data={{
+        labels: ['Commits', 'Pulls', 'Issues', 'Active Days'],
+        datasets: [
+          {
+            label: 'Target',
+            backgroundColor: 'rgba(220, 220, 220, 0.2)',
+            borderColor: 'rgba(220, 220, 220, 1)',
+            pointBackgroundColor: 'rgba(220, 220, 220, 1)',
+            pointBorderColor: '#fff',
+            pointHighlightFill: '#fff',
+            pointHighlightStroke: 'rgba(220, 220, 220, 1)',
+            data: [100, 100, 100, 100],
+          },
+          {
+            label: 'Current Progress',
+            backgroundColor: 'rgba(151, 187, 205, 0.2)',
+            borderColor: 'rgba(151, 187, 205, 1)',
+            pointBackgroundColor: 'rgba(151, 187, 205, 1)',
+            pointBorderColor: '#fff',
+            pointHighlightFill: '#fff',
+            pointHighlightStroke: 'rgba(151, 187, 205, 1)',
+            data: [125, 120, 40, 19],
+          },
+        ],
+      }}
+    />
+  )
 }
 
 export default Dashboard
