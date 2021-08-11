ae8f2c43f2150a1a98640ae092ff82bc442c1506
darin3 <darinh@rams.colostate.edu>
2021-08-10 18:48:16 -0600

    Move Callout logic to new file

---
+++ frontend/src/components/dashboard/MetricCallout.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { CCallout } from '@coreui/react'
+
+const MetricCallout = ({ label, value }) => {
+  return (
+    <CCallout color="dark" className="bg-white">
+      <small className="text-muted">{label}</small>
+      <br />
+      <strong className="h5">{value || '-'}</strong>
+    </CCallout>
+  )
+}
+
+MetricCallout.propTypes = {
+  label: PropTypes.string.isRequired,
+  value: PropTypes.number,
+}
+
+export default MetricCallout
